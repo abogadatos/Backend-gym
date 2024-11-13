c92fbed41c0ba5cb6ba72a521ee7feb7063e497b
angel <almazanluis23@gmail.com>
2024-11-13 16:45:24 -0600

    Se agregan cambios, creacion carpeta classes y Reviews

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -8,16 +8,14 @@ import {
   Delete,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  create() {
+    return this.authService.create();
   }
 
   @Get()
@@ -31,8 +29,8 @@ export class AuthController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
+  update(@Param("id")id ) {
+    return this.authService.update(id );
   }
 
   @Delete(':id')
---
+++ src/modules/reviews/reviews.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ReviewsService {
+    deleteReviews() {
+        return 'This action adds a new user';
+    }
+    updateReviews() {
+        return 'This action adds a new user';
+    }
+    postReviews() {
+        return 'This action adds a new user';
+    }
+    getByidReviews() {
+        return 'This action adds a new user';
+    }
+    getReviews() {
+        return 'This action adds a new user';
+    }
+    
+
+    }
+
