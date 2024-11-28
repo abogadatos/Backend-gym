7dc93af0690eb0a85cfae3af6bce543ecee31710
Anahi Diaz <ad955180@gmail.com>
2024-11-28 20:07:46 -0300

    config del schedule

--- src/modules/classes/dto/updateSchedule.dto.ts
+++ src/modules/classes/dto/updateSchedule.dto.ts
@@ -11,7 +11,7 @@ export class UpdateClassScheduleDto {
     })
     startTime?: string;
   
-    @IsOptional()
+    @IsOptional()   
     @IsString()
     @Matches(/^([01]\d|2[0-3]):([0-5]\d)$/, {
       message: 'endTime must be in HH:mm format',
