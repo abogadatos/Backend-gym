d1f1319fd8e282838c2b73d2eb2d91fbda110a60
angel <almazanluis23@gmail.com>
2024-11-30 16:53:55 -0600

    Swagger general

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationExceptionFilter } from './filters/validation-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,17 +20,15 @@ async function bootstrap() {
     credentials: true,
   });
 
-  //   const swaggerConfig = new DocumentBuilder()
-  //     .setTitle('ecommerce - nechodev API')
-  //     .setDescription(
-  //       'Ecommerce demo with NestJS for SoyHenry Bootcamp Module 4 project',
-  //     )
-  //     .setVersion('1.0')
-  //     .addBearerAuth()
-  //     .build();
+  const options = new DocumentBuilder()
+  .setTitle("Proyecto JustDoIt")
+  .setDescription("Proyecto dearrollado con NestJS")
+  .setVersion("1.0.0")
+  .addBearerAuth()
+  .build();
 
-  //   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  //   SwaggerModule.setup('docs', app, document);
+  const document = SwaggerModule.createDocument(app,options);
+  SwaggerModule.setup("api",app,document);
 
   await app.listen(process.env.APP_PORT);
   console.log(`Listening on port ${process.env.APP_PORT}`);
